ee2d7dd8019c18e5fd229664297e2e5f9d154755
james harder <james.harder@gmail.com>
2016-03-02 07:36:41 -0600

    Changes to be committed: 	modified:   jsketch.html 	modified:   scripts/jsketch.js 	modified:   styles/jsketch.css
    first version of jsketch

--- scripts/jsketch.js
+++ scripts/jsketch.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
- $('.pixel').hover(
+	for (var i = 0; i < resolution; i++) {
+		$(".container").append('<div class="pixel"></div>');
+	};
+	$(".pixel").width(pixelSize);
+	$(".pixel").height(pixelSize);
+	//console.log(pixelSize);
+	$('.pixel').hover(
 		function(){
 			$(this).css({"background": "#555"});
 		}, function() {
-			$(this).css({"background": "#369"});
+			//$(this).css({"background": "#369"});
 		}
 	);
+	$('button').on('click', function(){
+		$('.pixel').css({"background": "#369"})
+	});
 });
+
+var resolution = prompt("how many pixels?") ;
+var pixelSize = (480/Math.sqrt(resolution) - 2) ;
+//console.log(pixelSize);
+//var $container = $(".container")

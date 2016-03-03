4f9347d39fa3ea05acf4c340247860c9a64ab870
james harder <james.harder@gmail.com>
2016-03-02 23:53:51 -0600

    Changes to be committed: 	modified:   jsketch.html 	modified:   scripts/jsketch.js 	modified:   styles/jsketch.css
    version 0.0.2
    *added some abstraction
    *follows project guidlines more closely
    
    TODO
    *add different colors
    *add click handler to .pixel

--- scripts/jsketch.js
+++ scripts/jsketch.js
@@ -1,23 +1,43 @@
 $(document).ready(function() {
-	for (var i = 0; i < resolution; i++) {
+	$('#reset').on('click', function(){
+		console.log("setGrid returned " + setGrid(prompt("how many rows?")));
+		console.log("clearColors returned " + clearColors());
+	});
+	$('#clearColors').on('click', function() {
+		console.log("clearColors returned " + clearColors());
+	});
+	/*$('.pixel').hover(
+		function(){
+			$(this).css({"background": "#555"});
+		}, function() {
+			$(this).css({"background": "#333"});
+		}
+	);*/
+});
+
+function setGrid(gridSize) {
+	var numberOfPixels = gridSize * gridSize ;
+	var pixelSize = (480/(gridSize) - 2) ;
+	$('.pixel').remove();
+	for (var i = 0; i < numberOfPixels; i++) {
 		$(".container").append('<div class="pixel"></div>');
 	};
 	$(".pixel").width(pixelSize);
 	$(".pixel").height(pixelSize);
 	//console.log(pixelSize);
+	
 	$('.pixel').hover(
 		function(){
 			$(this).css({"background": "#555"});
 		}, function() {
-			//$(this).css({"background": "#369"});
+			$(this).css({"background": "#333"});
 		}
 	);
-	$('button').on('click', function(){
-		$('.pixel').css({"background": "#369"})
-	});
-});
+	
+	return true;
+};
 
-var resolution = prompt("how many pixels?") ;
-var pixelSize = (480/Math.sqrt(resolution) - 2) ;
-//console.log(pixelSize);
-//var $container = $(".container")
+function clearColors() {
+	$('.pixel').css({"background": "#369"});
+	return true;
+};
